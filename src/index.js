0fcc900e4fed59b66744fad65d757b2d0ac5dc19
ahmed-codics <ahmedismansour000@gmail.com>
2025-04-04 16:51:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ connectDB();
 // ✅ Add middleware BEFORE routes
 app.use(
     cors({
-        origin: true, // ✅ Allow requests from your frontend
+        origin: "*", // ✅ Allow requests from your frontend
         credentials: true, // ✅ Allow cookies & session authentication
         methods: ["GET", "POST", "PUT", "DELETE"], // ✅ Allow necessary HTTP methods
         allowedHeaders: ["Content-Type", "Authorization"] // ✅ Allow necessary headers
