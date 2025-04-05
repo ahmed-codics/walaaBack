dfc7663108c093b3caf660b080aeab875d81e6ae
ahmed-codics <ahmedismansour000@gmail.com>
2025-04-06 01:09:33 +0200

    Update doctorRoutes.js

--- src/routes/doctorRoutes.js
+++ src/routes/doctorRoutes.js
@@ -1,14 +1,26 @@
-import express from 'express';
-import Doctor from '../models/DoctorModel.js';
-import { fetchDocs, getDoctor, addDoctor } from '../controllers/doctorController.js';
-import multer from 'multer';
-import path from 'path';
-import upload from '../config/multerConfig.js'; // Use the imported upload
+import express from 'express'
+import Doctor from '../models/DoctorModel.js'
+import { fetchDocs, getDoctor, addDoctor, deleteDoctor } from '../controllers/doctorController.js';
+import multer from 'multer'
+import path from 'path'
 
 const router = express.Router();
 
-router.get("/", fetchDocs);
-router.get("/:id", getDoctor);
+const storage = multer.diskStorage({
+    destination : (req , file , cb) => {
+        cb(null , "uploads/");
+    },
+    filename : (req , file , cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)); // Unique file name
+    },
+});
+
+const upload = multer({storage});
+
+router.get("/" , fetchDocs);
+router.get("/:id" , getDoctor);
 router.post("/", upload.single("image"), addDoctor);
+router.delete("/:id" , deleteDoctor);
+
 
 export default router;
