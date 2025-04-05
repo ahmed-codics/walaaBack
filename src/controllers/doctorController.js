7fc8c9e4c9a132a4bb2056cd281a7c5ccc59afae
ahmed-codics <ahmedismansour000@gmail.com>
2025-04-06 01:09:11 +0200

    Update doctorController.js

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -1,4 +1,5 @@
 import Doctor from "../models/DoctorModel.js"
+import mongoose from "mongoose";
 
 export const fetchDocs = async (req , res) => {
     try {
@@ -14,29 +15,48 @@ export const getDoctor = async (req, res) => {
         const doctor = await Doctor.findById(req.params.id);
         if (!doctor) res.status(404).json({message : "Doctor Not Found!"});
         res.json(doctor);
-    } catch (error) {
+    } catch (error) { 
         res.status(500).json({ message: "Server error" });
     }
 }
 
 export const addDoctor = async (req, res) => {
     try {
-        const {name , specialization , experience , contact} = req.body;
-        const imageUrl = req.file ? req.file.path : '';
+        const { name, specialization, experience, contact } = req.body;
 
         const newDoctor = new Doctor({
             name,
             specialization,
             experience,
             contact,
-            image: imageUrl,
+            image: req.file ? `/uploads/${req.file.filename}` : "",
         });
 
-        await newDoctor.save();
-        res.status(201).json(newDoctor);
+        const savedDoctor = await newDoctor.save();
+        res.status(201).json(savedDoctor);
     } catch (error) {
         console.error("❌ Error in addDoctor:", error); // Log the real error
         res.status(500).json({ message: "Error adding doctor", error });
     }
 };
 
+export const deleteDoctor = async (req , res) => {
+    try {
+        const {id} = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({message : "invalid ID format"});
+        }
+
+        const deletingDoctor = await Doctor.findByIdAndDelete(id);
+
+        if (!deletingDoctor) {
+            res.status(404).json({message : "Doctor Not Found!"})
+        }
+
+        res.json({message : "Doctor Deleted!"})
+
+    } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+    }
+}
