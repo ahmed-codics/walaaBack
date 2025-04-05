baa689897b4e2e970966b0c931e27ebfbe2b0843
ahmed-codics <ahmedismansour000@gmail.com>
2025-04-05 17:27:36 +0200

    Update doctorRoutes.js

--- src/routes/doctorRoutes.js
+++ src/routes/doctorRoutes.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.get("/", fetchDocs);
 router.get("/:id", getDoctor);
-router.post("/doctors", upload.single("image"), addDoctor);
+router.post("/", upload.single("image"), addDoctor);
 
 export default router;
