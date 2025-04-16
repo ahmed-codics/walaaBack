71595aa03980ac8d4d7db628c1acb7e3bbd28110
ahmed-codics <ahmedismansour000@gmail.com>
2025-04-16 03:16:46 +0200

    Update messageRoutes.js

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { sendMessage , getMessage , deleteMessage } from '../controllers/messageController.js'
+import { sendMessage , getMessage , deleteMessage , getChatUsers } from '../controllers/messageController.js'
 import { protectRoute } from '../middlewares/authMiddle.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/' , protectRoute, sendMessage);
 router.get('/convo' , protectRoute, getMessage);
 router.delete(':/messageId' , protectRoute, deleteMessage);
+router.get('/chats', protectRoute, getChatUsers);
 
 export default router;
